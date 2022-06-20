3779768b9a3f4a7ca535c4a0f4817b0a0cf70d6b
michaelchekwas <arconwuchekwa@gmail.com>
2022-06-20 01:52:59 +0100

    first commit

---
+++ basic calc.py
@@ -0,0 +1,21 @@
+num1 = eval(input("Enter number: "))
+num2 = eval(input("Enter number: "))
+op = input("Enter operator: ")
+
+if op == "+":
+    print("The answer is: ", num1 + num2)
+
+elif op == "-":
+    print("The answer is: ", num1 - num2)
+
+elif op == "*":
+    print("The answer is: ", num1 * num2)
+
+elif op == "/":
+    print("The answer is: ", num1 / num2)
+
+elif op == "**":
+    print("The answer is: ", num1 ** num2)
+
+else:
+    print("comrade no be so o")
